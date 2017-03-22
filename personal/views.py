c634ec94d3644f873b766d4592b2c6c3ec709f14
Haider <Halbassam1992@gmail.com>
2017-03-22 18:16:45 -0500

    First commit

---
+++ personal/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'content':['Want to contact me?, email me at blah@gmail.com']})
+
+
